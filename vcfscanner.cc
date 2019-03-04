ef87e79fb6b91883a0adb9720d6d024c16d90013
Damon Revoe <him@revl.org>
2019-03-04 17:48:39 -0500

    Produce test output in VCF format for easier diff

--- vcfscanner.cc
+++ vcfscanner.cc
@@ -25,7 +25,6 @@
 
 #include "vcfscanner.hh"
 
-
 void CVCFScanner::SetNewInputBuffer(const char* buffer, ssize_t buffer_size)
 {
     m_Buffer = buffer;
@@ -585,33 +584,17 @@ CVCFScanner::EParsingEvent CVCFScanner::ParseGenotype()
         m_DataLineParsingState = eEndOfDataLine; */
 }
 
-static const char vcf_file[] = R"(##fileformat=VCFv4.0
-##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
-#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	S-1	S-2	S-3
-1	100000	rs123;rs333	C	G	10	.	.	GT	0|1	1/.	1/0
-2	200000	.	C	G,T	.	PASS	NS=3;DP=14;AF=0.5;DB;H2	GT	0|0	0|1	1|2)";
-
-static const char* vcf_eof_ptr = vcf_file + sizeof(vcf_file) - 1;
-static const char* current_ptr = vcf_file;
-
-static ssize_t s_ReadVCFFile(void* target_buffer, size_t buffer_size)
-{
-    size_t remaining_size = vcf_eof_ptr - current_ptr;
-    if (buffer_size > remaining_size)
-        buffer_size = remaining_size;
-    memcpy(target_buffer, current_ptr, buffer_size);
-    current_ptr += buffer_size;
-    return buffer_size;
-}
-
 static char buffer[1];
 
-#define SET_NEW_BUFFER()                                                       \
-    vcf_scanner.SetNewInputBuffer(buffer, s_ReadVCFFile(buffer, sizeof(buffer)))
+static void s_ReadAndSetNewInputBuffer(CVCFScanner& vcf_scanner, FILE* input)
+{
+    vcf_scanner.SetNewInputBuffer(
+            buffer, fread(buffer, 1, sizeof(buffer), input));
+}
 
 #define RETRY_UNTIL_OK_OR_ERROR(method)                                        \
     while ((pe = vcf_scanner.method()) == CVCFScanner::eNeedMoreData)          \
-        SET_NEW_BUFFER();                                                      \
+        s_ReadAndSetNewInputBuffer(vcf_scanner, input);                        \
     if (pe == CVCFScanner::eOKWithWarnings) {                                  \
         for (const auto& warning : vcf_scanner.GetWarnings())                  \
             cerr << "Warning: " << warning.warning_message << endl;            \
@@ -621,68 +604,110 @@ static char buffer[1];
 #define RETRY_UNTIL_OK_OR_SKIP_LINE(method)                                    \
     RETRY_UNTIL_OK_OR_ERROR(method);                                           \
     if (pe == CVCFScanner::eError) {                                           \
-        cerr << "file.vcf:" << vcf_scanner.GetLineNumber() << ": "             \
-             << vcf_scanner.GetError().m_ErrorMessage << endl                  \
-             << endl;                                                          \
+        cerr << "<-ERR@" << vcf_scanner.GetLineNumber() << ": "                \
+             << vcf_scanner.GetError().m_ErrorMessage << endl;                 \
         return;                                                                \
     }
 
-static void ParseDataLine(CVCFScanner& vcf_scanner)
+static void ParseDataLine(CVCFScanner& vcf_scanner, FILE* input)
 {
     CVCFScanner::EParsingEvent pe;
 
-    cout << '#' << vcf_scanner.GetLineNumber() << endl;
+    cout << vcf_scanner.GetLineNumber() << ':';
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseLoc);
-    cout << "CHROM:[" << vcf_scanner.GetChrom() << ']' << endl
-         << "POS:[" << vcf_scanner.GetPos() << ']' << endl;
+    cout << vcf_scanner.GetChrom() << '\t' << vcf_scanner.GetPos();
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseIDs);
-    for (const auto& id : vcf_scanner.GetIDs())
-        cout << "ID:[" << id << ']' << endl;
+    const char* sep;
+    if (!vcf_scanner.GetIDs().empty()) {
+        sep = "\t";
+        for (const auto& id : vcf_scanner.GetIDs()) {
+            cout << sep << id;
+            sep = ",";
+        }
+        cout << '\t';
+    } else
+        cout << "\t.\t";
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseAlleles);
-    cout << "REF:[" << vcf_scanner.GetRef() << ']' << endl;
-    for (const auto& alt : vcf_scanner.GetAlts())
-        cout << "ALT:[" << alt << ']' << endl;
+    cout << vcf_scanner.GetRef();
+    if (!vcf_scanner.GetAlts().empty()) {
+        sep = "\t";
+        for (const auto& alt : vcf_scanner.GetAlts()) {
+            cout << sep << alt;
+            sep = ",";
+        }
+        cout << '\t';
+    } else
+        cout << "\t.\t";
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseQuality);
     string quality = vcf_scanner.GetQuality();
     if (!quality.empty())
-        cout << "QUAL:[" << quality << ']' << endl;
+        cout << quality;
+    else
+        cout << ".";
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseFilters);
-    for (const auto& filter : vcf_scanner.GetFilters())
-        cout << "FILTER:[" << filter << ']' << endl;
+    if (!vcf_scanner.GetFilters().empty()) {
+        sep = "\t";
+        for (const auto& filter : vcf_scanner.GetFilters()) {
+            cout << sep << filter;
+            sep = ";";
+        }
+    } else
+        cout << "\t.";
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseInfo);
-    for (const auto& info_item : vcf_scanner.GetInfo())
-        cout << "INFO:[" << info_item << ']' << endl;
+    if (!vcf_scanner.GetInfo().empty()) {
+        sep = "\t";
+        for (const auto& info_item : vcf_scanner.GetInfo()) {
+            cout << sep << info_item;
+            sep = ";";
+        }
+        cout << '\t';
+    } else
+        cout << "\t.\t";
 
     RETRY_UNTIL_OK_OR_SKIP_LINE(ParseGenotypeFormat);
+    cout << "GT";
 
     vector<int> alleles;
 
     if (!vcf_scanner.CaptureGT(&alleles)) {
-        cout << "ERR: no GT key" << endl;
+        cout << "\tERR: no GT key" << endl;
         return;
     }
 
     while (vcf_scanner.GenotypeAvailable()) {
         RETRY_UNTIL_OK_OR_SKIP_LINE(ParseGenotype);
-        cout << "---" << endl;
-        for (auto allele : alleles)
-            cout << "GT:[" << allele << ']' << endl;
+        sep = "\t";
+        for (auto allele : alleles) {
+            cout << sep << allele;
+            sep = vcf_scanner.IsPhasedGT() ? "|" : "/";
+        }
     }
 
     cout << endl;
 }
 
-int main()
+int main(int argc, const char* argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage %s VCF_FILE\n", *argv);
+        return 2;
+    }
+
+    FILE* input = fopen(argv[1], "rb");
+    if (input == nullptr) {
+        perror(argv[1]);
+        return 1;
+    }
+
     CVCFScanner vcf_scanner;
 
-    SET_NEW_BUFFER();
+    s_ReadAndSetNewInputBuffer(vcf_scanner, input);
 
     CVCFScanner::EParsingEvent pe;
 
@@ -695,25 +720,24 @@ int main()
     }
 
     for (const auto& kv : vcf_scanner.GetHeader().GetMetaInfo()) {
-        cout << '[' << kv.first << ']' << endl;
         for (const auto& v : kv.second) {
-            cout << v << endl;
+            cout << kv.first << '=' << v << endl;
         }
     }
 
-    cout << endl << "[Sample IDs]" << endl;
+    cout << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT";
     for (const auto& v : vcf_scanner.GetHeader().GetSampleIDs()) {
-        cout << v << endl;
+        cout << '\t' << v;
     }
+    cout << endl;
 
-    cout << endl << "[Data Lines]" << endl;
     for (;;) {
         while (!vcf_scanner.Rewind())
-            SET_NEW_BUFFER();
+            s_ReadAndSetNewInputBuffer(vcf_scanner, input);
         if (vcf_scanner.AtEOF())
             break;
 
-        ParseDataLine(vcf_scanner);
+        ParseDataLine(vcf_scanner, input);
     }
 
     return 0;
